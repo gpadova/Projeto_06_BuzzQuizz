5f75c4f31fa3476108efa3e47f597c6a41ff5dce
Heitor Filgueiras <hsfilgueiras2@gmail.com>
2022-09-01 19:52:02 -0300

    antesDoPull

--- script.js
+++ script.js
@@ -19,3 +19,91 @@ function disponibilizarQuizzesNaTela(resposta){
         `
     }
 }
+
+let escolherTitulo;
+let escolherImagem;
+let nDePerguntas;
+let nDeNiveis;
+let arrayPerguntas;
+let arrayNiveis;
+
+function comecePeloComeco(){
+    escolherTitulo = document.querySelector(".titulo-do-quiz").value;
+    escolherImagem = document.querySelector(".url-da-imagem-quizz").value;
+    nDePerguntas = document.querySelector(".quantidade-perguntas-quizz").value;
+    nDeNiveis = document.querySelector(".quantidade-niveis-quiz").value;
+    criarPerguntas()
+}
+
+function criarPerguntas(){
+    
+}
+
+const quizTeste = {
+	title: "Título do quizz",
+	image: "https://http.cat/411.jpg",
+	questions: [
+		{
+			title: "Título da pergunta 1",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		},
+		{
+			title: "Título da pergunta 2",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		},
+		{
+			title: "Título da pergunta 3",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		}
+	],
+	levels: [
+		{
+			title: "Título do nível 1",
+			image: "https://http.cat/411.jpg",
+			text: "Descrição do nível 1",
+			minValue: 0
+		},
+		{
+			title: "Título do nível 2",
+			image: "https://http.cat/412.jpg",
+			text: "Descrição do nível 2",
+			minValue: 50
+		}
+	]
+}
